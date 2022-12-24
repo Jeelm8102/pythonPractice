3583be835e48c28fb3a04829a3392dfcab318cc4
Jeel <jeelmaheshwari812@gmail.com>
2022-12-24 19:08:15 +0530

    a program to print N numbers

---
+++ print_N_nos.py
@@ -0,0 +1,13 @@
+# number=int(input("Please Enter any number: "))
+# i=1
+# print("The list of natural number from 1 to",number)
+# while (i<=number):
+#     print(i)
+#     i+=1
+
+
+#using for loop
+num= int(input("Please Enter any number:"))
+print("\n Natural number from 1 to",num)
+for i in range (1,num+1):
+    print(i,end=" ")
