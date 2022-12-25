a0dbe90228b4ccaaee7c110ba66fd01f2339c1ff
Jeel <jeelmaheshwari812@gmail.com>
2022-12-25 19:26:08 +0530

    various ways of defining and using

---
+++ var_tut.py
@@ -0,0 +1,23 @@
+"""
+
+var1="Hello world!"
+var2=4
+var3=2.22
+var4="Python Tutorial"
+print(type(var3))
+print(var2 + var3)    #addition
+print(var1 + var4)    #concatnation
+print(var1 + var2)    #error
+
+"""
+#typecasting
+var1="35"
+var2="27"
+print(var1 + var2)
+print(int(var1) + int(var2))
+
+"""
+int()
+str()
+float()
+"""
